6a6c300bd15f4f027bd3bf349731119cea017a9f
Pradeepreddykapa <pradeepreddy@zelarsoft.com>
2022-03-27 15:10:54 +0530

    ss

--- K8/create.sh
+++ K8/create.sh
@@ -1,3 +1,3 @@
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash   #Incase you don't have k3d installed
-k3d cluster create skit-app
+# wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash   #Incase you don't have k3d installed
+k3d cluster create skit-app -p "31000:31000@loadbalancer"
 bash kube.sh
